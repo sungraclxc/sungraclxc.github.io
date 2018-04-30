969b42aea65794e8a0c9ce7dafce408f7b438d07
sungraclxc <sungraclxc@163.com>
2018-04-30 22:59:05 +0800

    add two demos
    * resume template which used svg features;
    * The integration of a star move animation, applied with canvas, and right-bottom barrage;

---
+++ starNbarrager/js/main.js
@@ -0,0 +1,18 @@
+//ÐÇ¿Õ
+var clientH = document.documentElement.clientHeight;
+var clientW =  document.documentElement.clientWidth;
+$(function() {
+    $("#html5_3d_animation").html5_3d_animation({
+        window_width: clientW - '3',
+        window_height: clientH - '4',
+        window_background: 'black',
+        star_count: '1000',
+        star_color: '#FBFFAF',
+        star_depth: '100'
+    });
+});
+
+
+//µ¯Ä»
+
+
